7d30d4417cd99811dd30f5dcd71b6b3af786a504
Zohayb Shaikh <zohayb1996@gmail.com>
2018-08-15 13:45:54 -0400

    FIXED Saving ART to database, Also fixed showing Image after screenshot taken

--- components/ArtPostForm.js
+++ components/ArtPostForm.js
@@ -1,11 +1,7 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import {
-  FormLabel,
-  FormInput,
-  Text,
-} from 'react-native-elements';
-import { View, Image} from 'react-native';
+import { FormLabel, FormInput, Text } from 'react-native-elements';
+import { View, Image } from 'react-native';
 import Button from './Button';
 
 import { saveArt } from '../store/artReducer';
@@ -19,8 +15,8 @@ class ArtPostFormPresenTational extends Component {
       title: '',
       description: '',
       likes: 0,
-      user:{},
-      coverPhoto:null
+      user: {},
+      coverPhoto: null,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -30,7 +26,7 @@ class ArtPostFormPresenTational extends Component {
     this.setState({
       location: artObj.location,
       artPiece: artObj.artPiece,
-      coverPhoto: artObj.coverPhoto
+      coverPhoto: artObj.coverPhoto.uri,
     });
   }
 
@@ -45,8 +41,8 @@ class ArtPostFormPresenTational extends Component {
     return (
       <View style={styles.container}>
         <Image
-          style={{width: 50, height: 50}}
-          source={this.state.coverPhoto}
+          style={{ width: 50, height: 50 }}
+          source={{ uri: this.state.coverPhoto }}
         />
 
         <Text h1 style={styles.heading}>
@@ -89,7 +85,6 @@ const styles = {
 const mapDispatchToProps = dispatch => {
   return {
     addArt: artObj => dispatch(saveArt(artObj)),
-    
   };
 };
 
