3983820a7fc27244c19019a6f2546fbae5821276
Zohayb Shaikh <zohayb1996@gmail.com>
2018-08-16 14:18:00 -0400

    revert

--- components/SingleArtItem.js
+++ components/SingleArtItem.js
@@ -45,7 +45,7 @@ const styles = {
   info: {
     flexDirection: 'column',
     width: '50%',
-    alignItems: 'left',
+    alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
   },
