36157fd294d69b4a8d7e0d05c82c41636dad8ba0
Matt Dworkin <mdworkin21@gmail.com>
2018-08-16 14:52:12 -0400

    log in with FB and redirect to artFeed

--- components/Home.js
+++ components/Home.js
@@ -1,40 +1,125 @@
 import React, {Component} from "react";
 import { Text, View, Image, TouchableOpacity, Button } from "react-native";
-import {loginWithFacebook} from '../firebase/firebaseSetup'
+// import {loginWithFacebook} from '../firebase/firebaseSetup'
+// other shit
+import * as firebase from 'firebase';
+import firebaseConfig from '../secrets.js'
+import processFBData from '../utilities/getDataFromFB'
+import axios from 'axios'
+import domain from "../domain.js";
+import { signup } from '../store/userReducer';
+import {connect} from 'react-redux'
 
+// const Home = ({ navigation }) => (
 
-const Home = ({ navigation }) => (
-
-  <View style={styles.container}>
-    <Text style={styles.text}>GraftAR</Text>
-    <View style={styles.buttonContainer}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate(`LoginForm`)}
+//   <View style={styles.container}>
+//     <Text style={styles.text}>GraftAR</Text>
+//     <View style={styles.buttonContainer}>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={() => navigation.navigate(`LoginForm`)}
         
-      >
-        <Text style={styles.buttonText}>Log In </Text>
+//       >
+//         <Text style={styles.buttonText}>Log In </Text>
         
-      </TouchableOpacity>
+//       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('SignUpForm')}
-      >
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={() => navigation.navigate('SignUpForm')}
+//       >
+//         <Text style={styles.buttonText}>Sign Up</Text>
+//       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => loginWithFacebook()}
-      >
-        <Text style={styles.buttonText}>Sign Up with Facebook</Text>
-      </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={() => loginWithFacebook()}
+//       >
+//         <Text style={styles.buttonText}>Sign Up with Facebook</Text>
+//       </TouchableOpacity>
 
-    </View>
-  </View>
-);
+//     </View>
+//   </View>
+// );
 
+
+firebase.initializeApp(firebaseConfig);
+let newUser
+class Home extends Component{ 
+  constructor(){
+    super()
+    
+  }
+  
+  async componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+     if (user != null) {
+       newUser = processFBData(user)
+       return newUser
+      }
+    })    
+  }
+
+// async sendToDB(user){
+//    await axios.post(`${domain}/api/user/signup`, user);
+// }
+
+  async loginWithFacebook() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      "2112247582376014",
+      { permissions: ['public_profile', 'email'] }
+    );
+    
+    if (type === 'success') {
+      // Build Firebase credential with the Facebook access token.
+      await this.props.handleSignUp(newUser)
+      const credential = firebase.auth.FacebookAuthProvider.credential(token);
+      // Sign in with credential from the Facebook user.
+            firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {
+                    console.log(error)
+              // Handle Errors here.
+            });
+    }
+  }
+
+render(){
+ const {navigation} = this.props
+ return (
+      <View style={styles.container}>
+        <Text style={styles.text}>GraftAR</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate(`LoginForm`)}
+            
+          >
+            <Text style={styles.buttonText}>Log In </Text>
+            
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('SignUpForm')}
+          >
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              this.loginWithFacebook()
+              navigation.navigate('ArtFeed')
+            }
+          }
+          >
+            <Text style={styles.buttonText}>Sign Up with Facebook</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = {
   container: {
@@ -66,4 +151,15 @@ const styles = {
   }
 };
 
-export default Home 
+const mapDispatchToProps = dispatch => {
+  return {
+    handleSignUp: formData => {
+      return dispatch(signup(formData));
+    },
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
