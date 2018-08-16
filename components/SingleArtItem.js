e968acb3d1d3a09ee97f39ca27829dd99de26cb0
Zohayb Shaikh <zohayb1996@gmail.com>
2018-08-16 13:15:16 -0400

    added author to feed

--- components/SingleArtItem.js
+++ components/SingleArtItem.js
@@ -1,25 +1,29 @@
-import React from "react";
-import { Text, TouchableOpacity, View, Image, Button } from "react-native";
-import { withNavigation } from "react-navigation";
+import React from 'react';
+import { Text, TouchableOpacity, View, Image, Button } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
 const SingleArtItem = props => {
   const { navigation } = props;
+  const { title, likes, description, user, coverPhoto } = props.art;
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.artImage}
         onPress={() =>
           navigation.navigate(`SingleArtView`, {
-            art: props.art
+            art: props.art,
           })
         }
       >
-        <Image style={styles.artImage} source={{ uri: props.art.coverPhoto }} />
+        <Image style={styles.artImage} source={{ uri: coverPhoto }} />
       </TouchableOpacity>
       <View style={styles.info}>
-        <Text style={styles.artTitle}>{props.art.title}</Text>
-        <Text style={styles.artLikes}>{`Likes: ${props.art.likes}`}</Text>
-        <Text style={styles.artDescription}>{props.art.description}</Text>
+        <Text style={styles.artTitle}>{title}</Text>
+        <Text style={styles.artLikes}>{`Likes: ${likes}`}</Text>
+        <Text style={styles.artDescription}>{description}</Text>
+        <Text style={styles.artDescription}>
+          Created by: {user.firstName + ' ' + user.lastName}
+        </Text>
       </View>
     </View>
   );
@@ -28,23 +32,23 @@ const SingleArtItem = props => {
 const styles = {
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#FFF",
-    marginTop: 5
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    marginTop: 5,
   },
   artImage: {
-    width: "50%",
-    height: "100%"
+    width: '50%',
+    height: '100%',
   },
   info: {
-    flexDirection: "column",
-    width: "50%",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10
-  }
+    flexDirection: 'column',
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
 };
 
 export default withNavigation(SingleArtItem);
